ce870558d6493d3898742505927d4ff9bc896f3d
Ryanpez <ryansynez@gmail.com>
2023-09-22 17:42:15 -0500

    completed part 2 of candidate testing assignment. Added 4 more questions via arrays

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,6 +2,8 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 
@@ -13,9 +15,10 @@ var candidateAnswer = "";
 let feedback = "";
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? '
+, '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -25,20 +28,23 @@ candidateName = input.question("What is your name?: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswers[i] = input.question(questions[i]);
+}
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  feedback = 'Correct!'
-  console.log(feedback);
-
-} else  {
-  feedback = 'Incorrect.'
-  console.log(feedback)
+for (let i = 0; i < correctAnswers.length; i++) {
+  if (candidateAnswers[i] === correctAnswers[i]) {
+    feedback = 'Correct!'
+    console.log(feedback);
+  } else  {
+      feedback = 'Incorrect.'
+      console.log(feedback)
+  }
 }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -50,12 +56,12 @@ if (candidateAnswer === correctAnswer) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hello, ${candidateName}!`);
+  console.log(`Hello, ${candidateName}!\n`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
   
 }
-runProgram();
+
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
